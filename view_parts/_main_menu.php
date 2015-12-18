4fdfea92c0d616c327c092e9d93f03bcab830124
agarand <alexandre_307@hotmail.com>
2015-12-17 22:56:18 -0500

    v1.2

--- view_parts/_main_menu.php
+++ view_parts/_main_menu.php
@@ -1,10 +1,13 @@
 <?php
 $menu_date=array(
     'Accueil'=>'index.php',
-    'Contact'=>'contact.php'
+    'Contact'=>'contact.php',
+    'Inscription'=>'inscription.php',
+    'Dashboard'=>'dashboard.php'
 );
-foreach($menu_date as $menu_date => $daydata){
-    echo "<th>$daydata[jour]</th>";
+var_dump($menu_date);
+foreach($menu_date as $menu => $url){
+    echo "<li><a href=\"$url\">menu</li>";
 }
 
 ?>
