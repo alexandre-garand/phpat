2dd3fd08e86daf7eb32420fed7497e449f39296d
agarand <alexandre_307@hotmail.com>
2016-01-02 23:02:18 -0500

    v1.5 ajout du formulaire qui fonctionne

--- view_parts/_inscription_form.php
+++ view_parts/_inscription_form.php
@@ -1,8 +1,9 @@
-<form><?php
-    echo 'Nom :';
-    echo 'Prenom :';
-    echo 'Courriel :';
-    echo 'Pseudonyme :';
-    echo 'Mot de passe :';
+<form action="../inscription.php.php" method="post">
+        <p>Nom : <input type="text" name="nom" /></p>
+        <p>Prénom : <input type="text" name="prenom" /></p>
+    <p>Courriel : <input type="text" name="courriel" /></p>
+    <p>Pseudonyme : <input type="text" name="pseudo" /></p>
+    <p>Mot de passe : <input type="text" name="motdepasse" /> </p>
 
-?></form>
+        <p><input type="submit" value="OK"></p>
+    </form>
