33f6b09a413d67103d637bcd5ed5cd9e1b33448b
agarand <alexandre_307@hotmail.com>
2016-01-04 14:43:10 -0500

    Champs ajoutés avec les 5 données

--- view_parts/_inscription_form.php
+++ view_parts/_inscription_form.php
@@ -1,9 +1,17 @@
-<form action="../inscription.php.php" method="post">
-        <p>Nom : <input type="text" name="nom" /></p>
-        <p>Prénom : <input type="text" name="prenom" /></p>
-    <p>Courriel : <input type="text" name="courriel" /></p>
-    <p>Pseudonyme : <input type="text" name="pseudo" /></p>
-    <p>Mot de passe : <input type="text" name="motdepasse" /> </p>
+<?php
+var_dump($_POST);
+?>
 
-        <p><input type="submit" value="Inscription"></p>
-    </form>
+<form id="formulaire" method="post">
+    <label for="prenom">Prénom :</label>
+    <input type="text" id="prenom" name="prenom" value="<?php echo array_key_exists('prenom', $_POST) ? $_POST['prenom']:''?>"/>
+    <label for="nom">Nom de famille :</label>
+    <input type="text" id="nom" name="nom" value="<?php echo array_key_exists('nom', $_POST) ? $_POST['nom']:''?>"/>
+    <label for="courriel">Courriel :</label>
+    <input type="text" id="courriel" name="courriel" value="<?php echo array_key_exists('courriel', $_POST) ? $_POST['courriel']:''?>"/>
+    <label for="username">Username :</label>
+    <input type="text" id="username" name="username" value="<?php echo array_key_exists('username', $_POST) ? $_POST['username']:''?>"/>
+    <label for="password">Password :</label>
+    <input type="password" id="password" name="username" value="<?php echo array_key_exists('password', $_POST) ? $_POST['password']:''?>"/>
+    <input type="submit" value="Inscrire"/>
+</form>
